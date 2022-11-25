499e98b9c277a64fa71506744226f6d7b56f0031
Sufiyan-tech <muhammadsufiyan987@gmail.com>
2022-11-25 12:06:18 +0500

    jenkins shared libraries

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,3 @@
-def call(String name , String description){
-    bat "echo hello ${name} and ${description}"
+def call(Map config [:]){
+    bat "echo hello ${config.name} and ${config.description}"
 }
