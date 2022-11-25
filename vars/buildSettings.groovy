b0f563b5580979e5c5fad0331b9de3fcc19c4ee5
Sufiyan-tech <muhammadsufiyan987@gmail.com>
2022-11-25 12:42:03 +0500

    add BuildSettings

--- vars/buildSettings.groovy
+++ vars/buildSettings.groovy
@@ -8,34 +8,34 @@ class BuildSetting{
         this.currentBuildNo = currentBuildNo
     }
 
-    public void setBuildSetting(){
-        switch(this.buildNo){
-            case 0:
-                this.buildName = "Sufiyan Zero Build"
-                this.buildDescription = "Sufiyan Zero Description"
-                break
-            case {build > 0}:
-                this.buildName = "Sufiyan Positive Build"
-                this.buildDescription = "Sufiyan Positive Description"
-                break
-            case {build < 0}:
-                this.buildName = "Sufiyan Negative Build"
-                this.buildDescription = "Sufiyan Negative Description"        
-                break
-            default:
-                this.buildName = "Sufiyan "+this.currentBuildNo+" Build"
-                this.buildDescription = "Sufiyan "+this.currentBuildNo+" Description"        
-                break
+    // public void setBuildSetting(){
+    //     switch(this.buildNo){
+    //         case 0:
+    //             this.buildName = "Sufiyan Zero Build"
+    //             this.buildDescription = "Sufiyan Zero Description"
+    //             break
+    //         case {build > 0}:
+    //             this.buildName = "Sufiyan Positive Build"
+    //             this.buildDescription = "Sufiyan Positive Description"
+    //             break
+    //         case {build < 0}:
+    //             this.buildName = "Sufiyan Negative Build"
+    //             this.buildDescription = "Sufiyan Negative Description"        
+    //             break
+    //         default:
+    //             this.buildName = "Sufiyan "+this.currentBuildNo+" Build"
+    //             this.buildDescription = "Sufiyan "+this.currentBuildNo+" Description"        
+    //             break
 
-        }
-    }
+    //     }
+    // }
 
 }
 
 
 def call(int buildNo){
     bat "echo DD"
-    // BuildSetting bSetting = new BuildSetting(buildNo , currentBuild.number)
+     BuildSetting bSetting = new BuildSetting(buildNo , currentBuild.number)
     // bSetting.setBuildSetting()
     // currentBuild.displayName = bSetting.name
     // currentBuild.description = bSetting.description
