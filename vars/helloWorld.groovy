4503f87fe90fa64740d9c69a69f5fca3a21de3a7
Sufiyan-tech <muhammadsufiyan987@gmail.com>
2022-11-25 12:12:46 +0500

    jenkins shared libraries

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,18 @@
+class TestClass{
+    String mes
+
+    public TestClass(String mes){
+        this.mes = mes
+    }
+
+    public String getMessage(){
+        return this.mes
+    }
+
+}
+
 def call(Map config = [:]){
+    TestClass tc = new TestClass("Good Morning")
+    config.description = tc.getMessage()
     bat "echo hello ${config.name} and ${config.description}"
 }
