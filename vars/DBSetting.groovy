a2f6467ef37a5f77718af2071acb08f464589fcd
Sufiyan-tech <muhammadsufiyan987@gmail.com>
2022-11-29 16:00:29 +0500

    first commit

--- vars/DBSetting.groovy
+++
@@ -1,4 +0,0 @@
-def call(){
-    def command = ["cd C:\\URebal\\UR-PRO-Backend\\src\\DB Scripts\\Blueleaf && set PGPASSWORD=UR_DEV&& psql -h 192.168.2.51 -d check-db -U UR_DEV -f CreateFirm.sql"];
-    command.execute();
-}
